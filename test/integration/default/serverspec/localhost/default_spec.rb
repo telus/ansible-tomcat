d0f43847ce9032f37afc315382181ea7cfefa4e2
Nikki Mtz <iamthe.angrytoast@gmail.com>
2016-04-20 11:38:20 -0700

    its tomcat7, not tomcat

--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -2,17 +2,13 @@ require 'spec_helper'
 
 describe 'ansible-tomcat::default' do
 
-  describe package('tomcat') do
+  describe package('tomcat7') do
     it { should be_installed.by('apt') }
   end
 
   describe service('tomcat7') do
     it { should be_enabled }
     it { should be_running }
-  end
-
-  describe port(8005) do
-    it { should be_listening }
   end
 
 end
